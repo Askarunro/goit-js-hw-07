43b4116ac8bb0aed054e0af2dc5af5d865399ef0
Askarunro <paskar.y.a@gmail.com>
2021-11-14 00:16:55 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 const galleryContainar = document.querySelector('.gallery');
 const cardsMarkup = createImgCardsMarkup(galleryItems);
 galleryContainar.insertAdjacentHTML('beforeend', cardsMarkup);
@@ -11,37 +8,39 @@ galleryContainar.insertAdjacentHTML('beforeend', cardsMarkup);
 function createImgCardsMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+      return `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
     })
     .join('');
 }
 
 galleryContainar.addEventListener('click', onGalleryLinkClick);
 
-console.log(galleryItems);
-
 function onGalleryLinkClick(evt) {
   evt.preventDefault();
-  const urlLinkOriginalImg = evt.target.dataset.source;
+
+  const galleryImg = evt.target;
+  const parentGalleryImg = galleryImg.closest('.gallery__item').href;
   const isGalleryLinkEl = evt.target.classList.contains('gallery__image');
   if (!isGalleryLinkEl) {
     return;
   }
 
-  const instance = basicLightbox.create(
-    `<img src="${urlLinkOriginalImg}" width="800" height="600">`,
-  );
-
-  instance.show();
-
-  return urlLinkOriginalImg;
+  return parentGalleryImg;
 }
+
+//  const gallery = galleryItems.simpleLightbox();
+//   const galleryItems = document.querySelector('.gallery li');
+//   gallery.next(); // Next Image
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captions: true,
+  //   captionsData: images.description,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  infinite: false,
+});
